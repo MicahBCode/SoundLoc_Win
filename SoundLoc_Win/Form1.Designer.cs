3166d80079486a9de24e4dba64af301ff6f718de
MicahBCode <mischa.binder@outlook.de>
2021-02-10 20:27:14 +0100

    working calculations with fix coordinates

--- SoundLoc_Win/Form1.Designer.cs
+++ SoundLoc_Win/Form1.Designer.cs
@@ -30,14 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.cB_COMPort = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(22, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 29);
+            this.button1.Size = new System.Drawing.Size(182, 56);
             this.button1.TabIndex = 0;
             this.button1.Text = "Verbinden";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,19 +46,29 @@
             // cB_COMPort
             // 
             this.cB_COMPort.FormattingEnabled = true;
-            this.cB_COMPort.Location = new System.Drawing.Point(107, 16);
+            this.cB_COMPort.Location = new System.Drawing.Point(214, 31);
+            this.cB_COMPort.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cB_COMPort.Name = "cB_COMPort";
-            this.cB_COMPort.Size = new System.Drawing.Size(92, 21);
+            this.cB_COMPort.Size = new System.Drawing.Size(180, 33);
             this.cB_COMPort.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 85);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1216, 641);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 384);
+            this.ClientSize = new System.Drawing.Size(1250, 738);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.cB_COMPort);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -69,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox cB_COMPort;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- SoundLoc_Win/Form1.cs
+++ SoundLoc_Win/Form1.cs
@@ -13,9 +13,31 @@ namespace SoundLoc_Win
 {
     public partial class Form1 : Form
     {
+        struct s_Microphone
+        {
+            public double d_xCoords;
+            public double d_yCoords;
+
+            public double d_time;
+            public double d_radius;
+        }
+
+        s_Microphone Microphone_1;
+        s_Microphone Microphone_2;
+        s_Microphone Microphone_3;
+
         public Form1()
         {
             InitializeComponent();
+
+            Microphone_1.d_xCoords = 0;
+            Microphone_1.d_yCoords = 100;
+
+            Microphone_2.d_xCoords = 0;
+            Microphone_2.d_yCoords = 0;
+
+            Microphone_3.d_xCoords = 100;
+            Microphone_3.d_yCoords = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,6 +47,53 @@ namespace SoundLoc_Win
             {
                 cB_COMPort.Items.Add(comport);
             }
+
+            Microphone_1.d_time = 1.0; // time in ms
+            Microphone_2.d_time = 0;
+            Microphone_3.d_time = 0.5;
+
+            int i_v = 340; // 340 m/s
+
+            richTextBox1.AppendText("Input Time microphone 1: " + Microphone_1.d_time.ToString() + "ms\r\n");
+            richTextBox1.AppendText("Input Time microphone 2: " + Microphone_2.d_time.ToString() + "ms\r\n");
+            richTextBox1.AppendText("Input Time microphone 3: " + Microphone_3.d_time.ToString() + "ms\r\n");
+
+            Microphone_1.d_radius = i_v * ( Microphone_1.d_time / 10 );
+            Microphone_2.d_radius = i_v * ( Microphone_2.d_time / 10 );
+            Microphone_3.d_radius = i_v * ( Microphone_3.d_time / 10 );
+
+            richTextBox1.AppendText("\n");
+
+            richTextBox1.AppendText("Distance 1: " + Microphone_1.d_radius.ToString() + "cm\r\n");
+            richTextBox1.AppendText("Distance 2: " + Microphone_2.d_radius.ToString() + "cm\r\n");
+            richTextBox1.AppendText("Distance 3: " + Microphone_3.d_radius.ToString() + "cm\r\n");
+
+            calculateSoundCoords(Microphone_1, Microphone_2, Microphone_3);
+        }
+
+        private void calculateSoundCoords(s_Microphone mic_1, s_Microphone mic_2, s_Microphone mic_3)
+        {
+            double x;
+            double y;
+            double r = (Math.Sqrt(-1 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 20000) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + Math.Pow(mic_2.d_radius, 2) - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000)) * Math.Abs(mic_1.d_radius - mic_2.d_radius) - Math.Pow(mic_1.d_radius, 4) + 2 * Math.Pow(mic_1.d_radius, 3) * mic_2.d_radius + 10000 * Math.Pow(mic_1.d_radius, 2) - mic_1.d_radius * (3 * Math.Pow(mic_2.d_radius, 3) - Math.Pow(mic_2.d_radius, 2) * mic_3.d_radius - mic_2.d_radius * (Math.Pow(mic_3.d_radius, 2) - 10000) + mic_3.d_radius * (Math.Pow(mic_3.d_radius, 2) - 10000)) + mic_2.d_radius * (2 * Math.Pow(mic_2.d_radius, 3) - Math.Pow(mic_2.d_radius, 2) * mic_3.d_radius - mic_2.d_radius * Math.Pow(mic_3.d_radius, 3) + mic_3.d_radius * (Math.Pow(mic_3.d_radius, 2) - 10000))) / (2 * (mic_1.d_radius - mic_2.d_radius) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + 2 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000));
+            if( r < 0)
+            {
+                r = -1 * (Math.Sqrt(-1 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 20000) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + Math.Pow(mic_2.d_radius, 2) - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000)) * Math.Abs(mic_1.d_radius - mic_2.d_radius) + Math.Pow(mic_1.d_radius, 4) - 2 * Math.Pow(mic_1.d_radius, 3) * mic_2.d_radius - 10000 * Math.Pow(mic_1.d_radius, 2) + mic_1.d_radius * (3 * Math.Pow(mic_2.d_radius, 3) - Math.Pow(mic_2.d_radius, 2) * mic_3.d_radius - mic_2.d_radius * (Math.Pow(mic_3.d_radius, 2) - 10000) + mic_3.d_radius * (Math.Pow(mic_3.d_radius, 2) - 10000)) - mic_2.d_radius * (2 * Math.Pow(mic_2.d_radius, 3) - Math.Pow(mic_2.d_radius, 2) * mic_3.d_radius - mic_2.d_radius * Math.Pow(mic_3.d_radius, 2) + mic_3.d_radius * (Math.Pow(mic_3.d_radius, 2) - 10000))) / (2 * (mic_1.d_radius - mic_2.d_radius) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + 2 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000));
+
+                x = -1 * (Math.Sqrt(-1 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 20000) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + Math.Pow(mic_2.d_radius, 2) - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000)) * Math.Abs(mic_1.d_radius - mic_2.d_radius) * (mic_2.d_radius - mic_3.d_radius) + Math.Pow(mic_1.d_radius, 4) * (mic_2.d_radius - mic_3.d_radius) - Math.Pow(mic_1.d_radius, 3) * (3 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius - Math.Pow(mic_3.d_radius, 2) + 10000) + Math.Pow(mic_1.d_radius, 2) * (3 * Math.Pow(mic_2.d_radius, 3) - mic_2.d_radius * (3 * Math.Pow(mic_3.d_radius, 2) - 20000) + 10000 * mic_3.d_radius) - mic_1.d_radius * (Math.Pow(mic_2.d_radius, 4) + 2 * Math.Pow(mic_2.d_radius, 3) * mic_3.d_radius - Math.Pow(mic_2.d_radius, 2) * (3 * Math.Pow(mic_3.d_radius, 2) - 20000) + 10000 * (Math.Pow(mic_3.d_radius, 2) - 10000)) + mic_2.d_radius * (Math.Pow(mic_2.d_radius, 3) * mic_3.d_radius - Math.Pow(mic_2.d_radius, 2) * (Math.Pow(mic_3.d_radius, 2) - 10000) - 10000 * mic_2.d_radius * mic_3.d_radius + 10000 * (Math.Pow(mic_3.d_radius, 2) - 10000)));
+                x /= 200 * (mic_1.d_radius - mic_2.d_radius) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + 2 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000);
+
+                y = Math.Sqrt(-1 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 20000) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + Math.Pow(mic_2.d_radius, 2) - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000)) * Math.Abs(mic_1.d_radius - mic_2.d_radius) - (Math.Pow(mic_1.d_radius, 2) - mic_1.d_radius * (mic_2.d_radius + mic_3.d_radius) + mic_2.d_radius * mic_3.d_radius - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000);
+                y /= 200 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + 2 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000);
+            } else
+            {
+                x = (Math.Sqrt(-1 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 20000) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + Math.Pow(mic_2.d_radius, 2) - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000)) * Math.Abs(mic_1.d_radius - mic_2.d_radius) * (mic_2.d_radius - mic_3.d_radius) - Math.Pow(mic_1.d_radius, 4) * (mic_2.d_radius - mic_3.d_radius) + Math.Pow(mic_1.d_radius, 3) * (3 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius - Math.Pow(mic_3.d_radius, 2) + 10000) - Math.Pow(mic_1.d_radius, 2) * (3 * Math.Pow(mic_2.d_radius, 3) - mic_2.d_radius * (3 * Math.Pow(mic_3.d_radius, 2) - 20000) + 10000 * mic_3.d_radius) + mic_1.d_radius * (Math.Pow(mic_2.d_radius, 4) + 2 * Math.Pow(mic_2.d_radius, 3) * mic_3.d_radius - Math.Pow(mic_2.d_radius, 3) - 20000) + 10000 * (Math.Pow(mic_3.d_radius, 2) - 10000)) - mic_2.d_radius * (Math.Pow(mic_2.d_radius, 3) * mic_3.d_radius - Math.Pow(mic_2.d_radius, 2) * (Math.Pow(mic_3.d_radius, 2) - 10000) - 10000 * mic_2.d_radius * mic_3.d_radius + 10000 * (Math.Pow(mic_3.d_radius, 2) - 10000));
+                x /= 200 * (mic_1.d_radius - mic_2.d_radius) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + 2 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000);
+
+                y = -1 * (Math.Sqrt(-1 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 20000) * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius * mic_2.d_radius + Math.Pow(mic_2.d_radius, 2) - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000)) * Math.Abs(mic_1.d_radius - mic_2.d_radius) + (Math.Pow(mic_1.d_radius, 2) - mic_1.d_radius * (mic_2.d_radius + mic_3.d_radius) + mic_2.d_radius * mic_3.d_radius - 10000) * (Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000));
+                y /= 200 * (Math.Pow(mic_1.d_radius, 2) - 2 * mic_1.d_radius + mic_2.d_radius + 2 * Math.Pow(mic_2.d_radius, 2) - 2 * mic_2.d_radius * mic_3.d_radius + Math.Pow(mic_3.d_radius, 2) - 10000);
+            }
+            richTextBox1.AppendText("Got r of: " + r.ToString() + "cm\r\n");
         }
     }
 }
